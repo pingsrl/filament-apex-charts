6977d7f2fea726a287a666f5e212bdb1dc871439
Giacomo Trezzi <giacomo.trezzi@gmail.com>
2025-08-20 15:46:33 +0200

    filament 4

--- src/Concerns/CanFilter.php
+++ src/Concerns/CanFilter.php
@@ -3,122 +3,122 @@
 namespace Leandrocfe\FilamentApexCharts\Concerns;
 
 use Filament\Forms\Concerns\InteractsWithForms;
-use Filament\Support\Enums\MaxWidth;
+use Filament\Support\Enums\Width;
 use Illuminate\Support\Arr;
 
 trait CanFilter
 {
-    use InteractsWithForms;
+	use InteractsWithForms;
 
-    protected static MaxWidth|string $filterFormWidth = MaxWidth::ExtraSmall;
+	protected static Width|string $filterFormWidth = Width::ExtraSmall;
 
-    public ?string $filter = null;
+	public ?string $filter = null;
 
-    public ?array $filterFormData = [];
+	public ?array $filterFormData = [];
 
-    public bool $dropdownOpen = false;
+	public bool $dropdownOpen = false;
 
-    /**
-     * Retrieve the path for the form state.
-     *
-     * @return string The path for the form state.
-     */
-    protected function getFormStatePath(): string
-    {
-        return 'filterFormData';
-    }
+	/**
+	 * Retrieve the path for the form state.
+	 *
+	 * @return string the path for the form state
+	 */
+	protected function getFormStatePath(): string
+	{
+		return 'filterFormData';
+	}
 
-    /**
-     * Retrieves the simple filter options.
-     *
-     * @return array|null The simple filter options.
-     */
-    protected function getFilters(): ?array
-    {
-        return null;
-    }
+	/**
+	 * Retrieves the simple filter options.
+	 *
+	 * @return array|null the simple filter options
+	 */
+	protected function getFilters(): ?array
+	{
+		return null;
+	}
 
-    /**
-     * Update the filter and emit an event with the updated options.
-     */
-    public function updatedFilter(): void
-    {
-        $this->dispatch('updateOptions', options: $this->getOptions())
-            ->self();
-    }
+	/**
+	 * Update the filter and emit an event with the updated options.
+	 */
+	public function updatedFilter(): void
+	{
+		$this->dispatch('updateOptions', options: $this->getOptions())
+			->self();
+	}
 
-    /**
-     * Retrieves the form schema.
-     *
-     * @return array The form schema.
-     */
-    protected function getFormSchema(): array
-    {
-        return [];
-    }
+	/**
+	 * Retrieves the form schema.
+	 *
+	 * @return array the form schema
+	 */
+	protected function getFormSchema(): array
+	{
+		return [];
+	}
 
-    /**
-     * Submit the filters form.
-     */
-    public function submitFiltersForm(): void
-    {
-        $this->form->validate();
+	/**
+	 * Submit the filters form.
+	 */
+	public function submitFiltersForm(): void
+	{
+		$this->form->validate();
 
-        $this->dispatch('updateOptions', options: $this->getOptions())
-            ->self();
+		$this->dispatch('updateOptions', options: $this->getOptions())
+			->self();
 
-        $this->dispatch('apexhcharts-dropdown', open: false);
-    }
+		$this->dispatch('apexhcharts-dropdown', open: false);
+	}
 
-    /**
-     * Reset the filters form.
-     */
-    public function resetFiltersForm(): void
-    {
-        $this->form->fill();
-        $this->form->validate();
+	/**
+	 * Reset the filters form.
+	 */
+	public function resetFiltersForm(): void
+	{
+		$this->form->fill();
+		$this->form->validate();
 
-        $this->dispatch('updateOptions', options: $this->getOptions())
-            ->self();
+		$this->dispatch('updateOptions', options: $this->getOptions())
+			->self();
 
-        $this->dispatch('apexhcharts-dropdown', open: false);
-    }
+		$this->dispatch('apexhcharts-dropdown', open: false);
+	}
 
-    /**
-     * Retrieves the count of indicators.
-     *
-     * @return int The count of indicators.
-     */
-    public function getIndicatorsCount(): int
-    {
-        if ($this->getFilterFormAccessible()) {
-            return count(
-                Arr::where($this->filterFormData, function ($value) {
-                    return $value !== null;
-                })
-            );
-        }
+	/**
+	 * Retrieves the count of indicators.
+	 *
+	 * @return int the count of indicators
+	 */
+	public function getIndicatorsCount(): int
+	{
+		if ($this->getFilterFormAccessible()) {
+			return count(
+				Arr::where($this->filterFormData, function ($value) {
+					return null !== $value;
+				})
+			);
+		}
 
-        return 0;
-    }
+		return 0;
+	}
 
-    /**
-     * Retrieves the accessibility of the filter form data.
-     *
-     * @return bool The accessibility of the filter form data.
-     */
-    public function getFilterFormAccessible(): bool
-    {
-        return Arr::accessible($this->filterFormData) && count($this->filterFormData) > 0;
-    }
+	/**
+	 * Retrieves the accessibility of the filter form data.
+	 *
+	 * @return bool the accessibility of the filter form data
+	 */
+	public function getFilterFormAccessible(): bool
+	{
+		return Arr::accessible($this->filterFormData) && count($this->filterFormData) > 0;
+	}
 
-    /**
-     * Retrieves the value of the static property $filterFormWidth.
-     *
-     * @return MaxWidth | string The value of the $filterFormWidth property, which is either a MaxWidth instance or a string.
-     */
-    protected function getFilterFormWidth(): MaxWidth|string
-    {
-        return static::$filterFormWidth;
-    }
+	/**
+	 * Retrieves the value of the static property $filterFormWidth.
+	 *
+	 * @return maxWidth|string The value of the $filterFormWidth property, which is either a MaxWidth instance or a string
+	 */
+	protected function getFilterFormWidth(): Width|string
+	{
+		return static::$filterFormWidth;
+	}
 }
