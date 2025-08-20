c2502bf17399852583cc99726ff5d2c6c617da83
Giacomo Trezzi <giacomo.trezzi@gmail.com>
2025-08-20 15:46:09 +0200

    filament 4

--- resources/views/widgets/components/filter-form.blade.php
+++ resources/views/widgets/components/filter-form.blade.php
@@ -1,70 +1,124 @@
-@props(['indicatorsCount', 'width'])
+<?php
 
-@php
-    use Filament\Support\Enums\MaxWidth;
-@endphp
+namespace Leandrocfe\FilamentApexCharts\Concerns;
 
-<div class="filament-apex-charts-filter-form relative">
-    <div class="filament-dropdown-trigger cursor-pointer flex items-center justify-end" aria-expanded="false">
-        <button type="button" @click="dropdownOpen = !dropdownOpen"
-            class="fi-icon-btn relative flex items-center justify-center rounded-lg outline-none transition duration-75 focus:ring-2 disabled:pointer-events-none disabled:opacity-70 h-9 w-9 text-gray-400 hover:text-gray-500 focus:ring-primary-600 dark:text-gray-500 dark:hover:text-gray-400 dark:focus:ring-primary-500 fi-ac-icon-btn-action"
-            title="Filter">
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Support\Enums\Width;
+use Illuminate\Support\Arr;
 
-            <span class="sr-only">
-                Filter
-            </span>
+trait CanFilter
+{
+	use InteractsWithForms;
 
-            <x-filament::icon icon="heroicon-s-funnel" class="h-5 w-5" />
+	protected static Width|string $filterFormWidth = Width::ExtraSmall;
 
-            @if ($indicatorsCount > 0)
-                <div class="absolute start-full top-0 z-10 -ms-1 -translate-x-1/2 rounded-md bg-white dark:bg-gray-900">
-                    <div style="--c-50:var(--primary-50);--c-300:var(--primary-300);--c-400:var(--primary-400);--c-600:var(--primary-600);"
-                        class="fi-badge flex items-center justify-center gap-x-1 whitespace-nowrap rounded-md  text-xs font-medium ring-1 ring-inset px-0.5 min-w-[theme(spacing.4)] tracking-tighter bg-custom-50 text-custom-600 ring-custom-600/10 dark:bg-custom-400/10 dark:text-custom-400 dark:ring-custom-400/30">
+	public ?string $filter = null;
 
-                        <span>
-                            {{ $indicatorsCount }}
-                        </span>
+	public ?array $filterFormData = [];
 
-                    </div>
-                </div>
-            @endif
+	public bool $dropdownOpen = false;
 
-        </button>
-    </div>
+	/**
+	 * Retrieve the path for the form state.
+	 *
+	 * @return string the path for the form state
+	 */
+	protected function getFormStatePath(): string
+	{
+		return 'filterFormData';
+	}
 
-    <div x-show="dropdownOpen" x-cloak @click="dropdownOpen = false" class="fixed inset-0 h-full w-full z-10"></div>
+	/**
+	 * Retrieves the simple filter options.
+	 *
+	 * @return array|null the simple filter options
+	 */
+	protected function getFilters(): ?array
+	{
+		return null;
+	}
 
-    <div x-show="dropdownOpen" x-cloak @class([
-        'absolute mt-2 z-10 w-screen divide-y divide-gray-100 rounded-lg bg-white shadow-lg ring-1 ring-gray-950/5 transition dark:divide-gray-700 dark:bg-gray-800 dark:ring-white/20',
-    ])
-        style="{{ match ($width) {
-            MaxWidth::ExtraSmall, 'xs' => 'width: 20rem;',
-            MaxWidth::Small, 'sm' => 'width: 24rem;',
-            MaxWidth::Medium, 'md' => 'width: 28rem;',
-            MaxWidth::Large, 'lg' => 'width: 32rem;',
-            MaxWidth::ExtraLarge, 'xl' => 'width: 36rem;',
-            MaxWidth::TwoExtraLarge, '2xl' => 'width: 42rem;',
-            MaxWidth::ThreeExtraLarge, '3xl' => 'width: 48rem;',
-            MaxWidth::FourExtraLarge, '4xl' => 'width: 56rem;',
-            MaxWidth::FiveExtraLarge, '5xl' => 'width: 64rem;',
-            MaxWidth::SixExtraLarge, '6xl' => 'width: 72rem;',
-            MaxWidth::SevenExtraLarge, '7xl' => 'width: 80rem;',
-            default => $width,
-        } }}; right:0">
-        <div class="py-4 px-6">
+	/**
+	 * Update the filter and emit an event with the updated options.
+	 */
+	public function updatedFilter(): void
+	{
+		$this->dispatch('updateOptions', options: $this->getOptions())
+			->self();
+	}
 
-            {{ $slot }}
+	/**
+	 * Retrieves the form schema.
+	 *
+	 * @return array the form schema
+	 */
+	protected function getFormSchema(): array
+	{
+		return [];
+	}
 
-            <div class="mt-2 text-end flex gap-6 justify-end">
-                <x-filament::link wire:click="submitFiltersForm" color="primary" tag="button" size="sm">
-                    {{ __('filament-actions::modal.actions.submit.label') }}
-                </x-filament::link>
+	/**
+	 * Submit the filters form.
+	 */
+	public function submitFiltersForm(): void
+	{
+		$this->form->validate();
 
-                <x-filament::link wire:click="resetFiltersForm" color="danger" tag="button" size="sm">
-                    {{ __('filament-apex-charts::filters.reset.label') }}
-                </x-filament::link>
-            </div>
-        </div>
+		$this->dispatch('updateOptions', options: $this->getOptions())
+			->self();
 
-    </div>
-</div>
+		$this->dispatch('apexhcharts-dropdown', open: false);
+	}
+
+	/**
+	 * Reset the filters form.
+	 */
+	public function resetFiltersForm(): void
+	{
+		$this->form->fill();
+		$this->form->validate();
+
+		$this->dispatch('updateOptions', options: $this->getOptions())
+			->self();
+
+		$this->dispatch('apexhcharts-dropdown', open: false);
+	}
+
+	/**
+	 * Retrieves the count of indicators.
+	 *
+	 * @return int the count of indicators
+	 */
+	public function getIndicatorsCount(): int
+	{
+		if ($this->getFilterFormAccessible()) {
+			return count(
+				Arr::where($this->filterFormData, function ($value) {
+					return null !== $value;
+				})
+			);
+		}
+
+		return 0;
+	}
+
+	/**
+	 * Retrieves the accessibility of the filter form data.
+	 *
+	 * @return bool the accessibility of the filter form data
+	 */
+	public function getFilterFormAccessible(): bool
+	{
+		return Arr::accessible($this->filterFormData) && count($this->filterFormData) > 0;
+	}
+
+	/**
+	 * Retrieves the value of the static property $filterFormWidth.
+	 *
+	 * @return maxWidth|string The value of the $filterFormWidth property, which is either a MaxWidth instance or a string
+	 */
+	protected function getFilterFormWidth(): Width|string
+	{
+		return static::$filterFormWidth;
+	}
+}
